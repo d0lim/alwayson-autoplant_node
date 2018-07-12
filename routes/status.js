6e774a51912e702155dae5d5211bb2c3bc5ae492
Yoo Heebin <r.inc.md@gmail.com>
2018-07-12 14:51:08 +0900

    스테이터스, 시리얼 처리
    스테이터스에 5초마다 갱신되는 변수 처리함, 자세한 것은 시리얼 참조
    
    시리얼에다가 5초마다 갱신시키는 것, 물주기, 온도, 습도, 빛 관리할 수 있는 시리얼 함수 입력함.

--- routes/status.js
+++ routes/status.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var temperature;
+var humidity;
+var Soil;
+var Light;
 
 router.get('/',(req, res)=>{
   console.log('status!')
 })
 
 module.exports = router;
+module.exports.temperature=temperature;
+module.exports.humidity=humidity;
+module.exports.Soil=Soil;
+module.exports.Light=Light;
--- utils/serial.js
+++ utils/serial.js
@@ -1,9 +1,12 @@
 var SerialPort = require('serialport');
+var status= require('../routs/status')
+
 var port = new SerialPort('/dev/ttyACM0',{ //포트확인
 baudrate: 9600,
 parser: SerialPort.parsers.readline('\n')
 });
- 
+
+
 port.on('open', function() {
 console.log('opened')
 });
@@ -16,16 +19,128 @@ port.on('error', function(err) {
 
 port.on('data', function (data) {
 
+  switch (data.substring(0,3)) { 
+    case DHT: //DHT:temp10.00hum30.00
+      status.temperature=Number(data.substring(8,13))
+      status.humidity=Number(data.substring(16,21))
+      break;
+    case Soi://Soi:0~300~700~1024
+      status.Soil=Number(data.substring(4))
+      break;
+    case Rad://Rad:0~1024
+     status.Light=Number(data.substring(4))
+      break;
+
+  }
   console.log('Read and Send Data : ' + data);
 });
 
-
-/*
-  port.write('1', function(err) {
+module.exports.motion.waterOn=function(){
+  port.write('W1', function(err) {
     if (err) {
       return console.log('Error on write: ', err.message);
     }
-    //console.log('1 written');
   });
+}
+module.exports.motion.waterOff=function(){
+  port.write('W0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+
+}
+module.exports.motion.heaterOn=function(){
+  port.write('H1', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+
+}
+module.exports.motion.heaterOff=function(){
+  port.write('H0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+module.exports.motion.humidifierOn=function(){
+  port.write('U1', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+module.exports.motion.humidifierOff=function(){
+  port.write('U0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+module.exports.motion.LEDOn=function(){
+  port.write('L1', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+module.exports.motion.LEDOff=function(){
+  port.write('L0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+/* 센서를 위해서 껐다켰다가 필요하다고 함.
+var SoilOn=function(){
+  port.write('L0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+
+var SoilOff=function(){
+  port.write('L0', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
+*/
+var DHT=function(){
+  port.write('DHT', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+
+  //return {temp : 11.2, hum : 10.0}
+}
+var Soil=function(){
+  port.write('Soi', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+  //return {soilHum : 10.0}
+}
+var Radiation=function(){
+  port.write('Rad', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+  //return {Light : 10.0}
+}
+
+setInterval(DHT, 5000);
+setInterval(Soil, 5000);
+setInterval(Radiation, 5000);
+
+/*
+
 
 */
