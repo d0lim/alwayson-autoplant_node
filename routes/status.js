2a2f8a6c3a6aae9be2e31ec1364fdc3d2a7ad328
Yoo Heebin <r.inc.md@gmail.com>
2018-10-28 15:05:29 +0900

    test

--- routes/status.js
+++ routes/status.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var temperature;
-var humidity;
-var Soil;
-var Light;
+
+var temperature=0; //도씨
+var humidity=0; //퍼센트
+var Soil=0; //0 ~ 1024 0~300~700~1024 분기로 습도 단계라고 함
+var Light=0; // 광량 0 ~ 1024
+            //항상 갱신되는 변수들, serial.js 에서 5000ms 마다 인터벌
 
 router.get('/',(req, res)=>{
   console.log('status!')
